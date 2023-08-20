200c5e0f514a2189da5e8f9e201394c7628a75a4
gokdenizozkan <gokdenizzkn@outlook.com>
2023-08-20 15:58:20 +0300

    added MinMaxFinder

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-import patikaplus.week1.ChineseZodiacFinder;
-import patikaplus.week1.LeapYearFinder;
+import patikaplus.week1.MinMaxFinder;
 
 public class Main {
     public static void main(String[] args) {
-        LeapYearFinder.run();
+        MinMaxFinder.run();
     }
 }
