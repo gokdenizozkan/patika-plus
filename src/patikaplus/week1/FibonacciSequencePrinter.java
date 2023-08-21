2f0d809d4c8315568657bcb5d2863a2e10372ec6
gokdenizozkan <gokdenizzkn@outlook.com>
2023-08-21 16:40:55 +0300

    added FibonacciSequencePrinter

---
+++ src/patikaplus/week1/FibonacciSequencePrinter.java
@@ -0,0 +1,24 @@
+package patikaplus.week1;
+
+import java.util.Scanner;
+
+public class FibonacciSequencePrinter {
+    public static void run() {
+        Scanner sc = new Scanner(System.in);
+        int priorNumber, previousNumber, currentNumber, sequenceLength;
+
+        priorNumber = 0;
+        previousNumber = 1;
+
+        System.out.println("Please enter the sequence length:");
+        sequenceLength = sc.nextInt();
+        for (int i = 0; i < sequenceLength; i++) {
+            currentNumber = priorNumber + previousNumber;
+
+            System.out.println(priorNumber + " + " + previousNumber + " = " +  currentNumber);
+
+            priorNumber = previousNumber;
+            previousNumber = currentNumber;
+        }
+    }
+}
