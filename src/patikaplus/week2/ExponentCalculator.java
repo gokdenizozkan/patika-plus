cdcc268fc0eeb3e2556b33337d66b78a34ecf866
gokdenizozkan <gokdenizzkn@outlook.com>
2023-08-22 10:37:31 +0300

    added ExponentCalculator.calculateExponentRecursively

--- src/patikaplus/week2/ExponentCalculator.java
+++ src/patikaplus/week2/ExponentCalculator.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
 public class ExponentCalculator {
     public static void run() {
         Scanner sc = new Scanner(System.in);
-        int base, exponent;
+        long base, exponent;
         System.out.println("Please enter the base value:");
-        base = sc.nextInt();
+        base = sc.nextLong();
 
         System.out.println("Please enter the exponent value:");
-        exponent = sc.nextInt();
+        exponent = sc.nextLong();
 
-        System.out.println(base + "^" + exponent + " = " + (long) (Math.pow(base, exponent)));
+        System.out.println(base + "^" + exponent + " = " + calculateExponentRecursively(base, exponent));
+    }
+
+    public static long calculateExponentRecursively(long base, long exponent) {
+        if (exponent == 1) {
+            return base;
+        }
+        return base * calculateExponentRecursively(base, exponent - 1);
     }
 }
