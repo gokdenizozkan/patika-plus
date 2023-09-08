77d208e2caa8cc9a641ffddf138266f4aadfe77b
gokdenizozkan <gokdenizzkn@outlook.com>
2023-09-08 18:26:37 +0300

    modified Input.java for combat system dev0.0.4

--- src/patikaplus/week4/nighter/util/Input.java
+++ src/patikaplus/week4/nighter/util/Input.java
@@ -1,15 +1,39 @@
 package patikaplus.week4.nighter.util;
 
+import patikaplus.week4.nighter.manag.GameManager.Fight.Action;
 import java.util.Scanner;
 
 public class Input {
-    private static Scanner sc = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
 
     public static int getSelection() {
-        return sc.nextInt();
+        // Returns the input by decreasing it by 1
+        return sc.nextInt() - 1;
     }
 
     public static String getLine() {
         return sc.nextLine();
+    }
+
+    public static class Player {
+        public static Action getAction() {
+            Print.options(Action.getNames());
+
+            int input = getSelection();
+            for (int i = 0; i < Action.length; i++) {
+                if (input == i) {
+                    return Action.getActions()[i];
+                }
+            }
+            return null;
+        }
+    }
+
+    public static class Ai {
+        public static Action getAction() {
+            return Action.getActions()
+                    [Tengri.range(
+                        Action.length)];
+        }
     }
 }
