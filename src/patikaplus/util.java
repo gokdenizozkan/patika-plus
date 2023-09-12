c539049c5f3bf81a8c66f99ba059a672554955a2
gokdenizozkan <gokdenizzkn@outlook.com>
2023-09-12 17:09:52 +0300

    added min, max methods to util.java

--- src/patikaplus/util.java
+++ src/patikaplus/util.java
@@ -30,4 +30,32 @@ public class util {
 
         return intArray;
     }
+
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+
+    public static int min(int[] arr) {
+        int m = arr[0];
+        for (int i : arr) {
+            if (i < m) {
+                m = i;
+            }
+        }
+        return m;
+    }
+
+    public static int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    public static int max(int[] arr) {
+        int m = arr[0];
+        for (int i : arr) {
+            if (i > m) {
+                m = i;
+            }
+        }
+        return m;
+    }
 }
