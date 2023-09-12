31236a44fdca7948e77c9373da89d9928d27e78b
gokdenizozkan <gokdenizzkn@outlook.com>
2023-09-12 16:48:49 +0300

    added HarmonicMeanCalculator

--- src/patikaplus/week2/HarmonicMeanCalculator.java
+++ src/patikaplus/week2/HarmonicMeanCalculator.java
@@ -2,7 +2,7 @@ package patikaplus.week2;
 
 import java.util.Scanner;
 
-public class ArrayHarmonicSeriesCalculator {
+public class HarmonicMeanCalculator {
     private static final Scanner sc = new Scanner(System.in);
 
     public static void run() {
