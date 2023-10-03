830c5801c0aa3ccaacf67cc5c445713b8f4f9cf7
Gökdeniz Özkan <gokdenizzkn@outlook.com>
2023-10-03 10:39:04 +0300

    added WordCounter

---
+++ src/patikaplus/week5/WordCounter.java
@@ -0,0 +1,38 @@
+package patikaplus.week5;
+
+import java.util.HashMap;
+
+public class WordCounter {
+    private static HashMap<String, Integer> wordCounts = new HashMap<>();
+    
+    public static void main(String[] args) {
+        String sentence = "Oh ma darling, oh-my darling, oh-my darling Clementine! You were lost and gone forever, dreadful sorrow Clementine.";
+        System.out.println(sentence);
+        System.out.println(getWordWithHighestFrequency(sentence));
+    }
+    
+    public static String getWordWithHighestFrequency(String str) {
+        String[] tokens = str.trim().strip().replace(",", "").replace(".", "").replace("!", "").toLowerCase().split(" ");
+        
+        int highestFreq = 0;
+        String highestFreqWord = "";
+        for (String t : tokens) {
+            // adding
+            if (wordCounts.containsKey(t)) {
+                wordCounts.put(t, wordCounts.get(t) + 1);
+            }
+            else {
+                wordCounts.put(t, 1);
+            }
+            
+            // freq calc
+            int wordFreq = wordCounts.get(t);
+            if (wordFreq > highestFreq) {
+                highestFreq = wordFreq;
+                highestFreqWord = t;
+            }
+        }
+        
+        return highestFreqWord;
+    }
+}
